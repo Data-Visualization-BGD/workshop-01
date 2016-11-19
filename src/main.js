6f46530656c4dbdadc5bcf433f1b21fed8ef17c2
Filip Jelic <jelic.filip@gmail.com>
2016-11-19 11:32:20 +0100

    added boilerplate

---
+++ src/main.js
@@ -0,0 +1,33 @@
+$(function () {
+
+    var DEFAULTS = {
+        tick_count: 10,
+        x_tick_count: 16,
+
+        top_circle_radius: 6,
+
+        brush_height: 200,
+
+        graph_width: 800,
+        graph_height: 500
+    };
+
+    var margin = {top: 20, right: 20, bottom: 50, left: 60},
+        width = DEFAULTS.graph_width - margin.left - margin.right,
+        height = DEFAULTS.graph_height - margin.top - margin.bottom;
+
+
+// append the svg object to the body of the page
+// append a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+    var svg = d3.select(".bar-chart").append("svg")
+        .attr("width", width + margin.left + margin.right + DEFAULTS.legend_width)
+        .attr("height", height + margin.top + margin.bottom + DEFAULTS.brush_height)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+
+    // GO GO GO :)
+
+})
